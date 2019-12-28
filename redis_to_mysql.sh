012b53d9cc5de9eedc712109f22cf6bfe6ea523e
CoderMSF <390355337@qq.com>
2019-12-28 20:28:02 +0800

    将redis的key-value迁移到mysql

--- redis_to_mysql.sh
+++ redis_to_mysql.sh
@@ -11,3 +11,11 @@ for key in "${arrIN[@]}";do
 	${MysqlLogin} -e "insert into test.redis_record(r_key,r_value) values('${key}','${value}');"
 done
 
+
+
+#CREATE TABLE `redis_record` (
+#  `id` int(8) NOT NULL AUTO_INCREMENT,
+#  `r_key` text NOT NULL,
+#  `r_value` text NOT NULL,
+#  PRIMARY KEY (`id`)
+#) ENGINE=InnoDB AUTO_INCREMENT=707 DEFAULT CHARSET=utf8
